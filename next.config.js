07f70bc15f837a451f9c6ea90e3293ca2d29c1fe
Oliver Butler <olly@protoncloud.co.uk>
2020-05-08 22:04:36 +0100

    🚧 Fixing session cookie secret

--- next.config.js
+++ next.config.js
@@ -13,5 +13,6 @@ module.exports = {
     POST_LOGOUT_REDIRECT_URI:
       process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/",
     SESSION_COOKIE_LIFETIME: 7200, // 2 hours
+    TEST: "Hello World!",
   },
 };
