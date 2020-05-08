7b31deea577e06b4da434eabbb8aa34abf55efca
Oliver Butler <olly@protoncloud.co.uk>
2020-05-08 22:32:10 +0100

    🙈 Add .now to .gitignore

--- pages/api/test.js
+++
@@ -1,7 +0,0 @@
-import getConfig from "next/config";
-
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-
-export default function test(req, res) {
-  res.send(serverRuntimeConfig.SESSION_COOKIE_SECRET);
-}
