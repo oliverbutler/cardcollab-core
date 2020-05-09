78be3527e02873fc7dffe5947ea195a4821d5568
Oliver Butler <olly@protoncloud.co.uk>
2020-05-09 14:38:50 +0100

    ⚗ Experimenting Mongoose Connection

--- middlewares/db.js
+++ middlewares/db.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 const connectDb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) return handler(req, res);
   // Using new database connection
-  await mongoose.connect(process.env.MONGODB_URI, {
+  await mongoose.connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
   });
-  await import("../models/user");
+  await import("../models/card.js");
+
   return handler(req, res);
 };
 
---
+++ models/card.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema({
+  content: { type: String, required: true },
+  sub: { type: String, required: true },
+});
+
+var Card = mongoose.model("Card", cardSchema);
+
+module.exports = Card;
