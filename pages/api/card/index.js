516f7b5fe50e156fb52ce69936defd60df370e53
Oliver Butler <olly@protoncloud.co.uk>
2020-05-11 15:41:28 +0100

    🐛 Fixed mongoose code

--- pages/api/card/index.js
+++ pages/api/card/index.js
@@ -1,51 +1,45 @@
 import mongoose from "mongoose";
 import dbConnect from "../../../middlewares/db";
 
-const { Card } = mongoose.models;
-
 const handler = async (req, res) => {
-  try {
-    switch (req.method) {
-      case "GET":
-        const cards = await Card.find(
-          req.query.q ? JSON.parse(req.query.q) : {}
-        );
-        res.send(cards);
-        break;
+  switch (req.method) {
+    case "GET":
+      const cards = await mongoose.models.Card.find(
+        req.query.q ? JSON.parse(req.query.q) : {}
+      );
+      res.send(cards);
+      break;
 
-      case "POST":
-        const newCard = new Card(req.body);
-        await newCard
-          .save()
-          .then(() => {
-            res.status(201).send(newCard);
-          })
-          .catch((error) => {
-            res.status(400).end();
-          });
-        break;
-
-      case "PUT":
-        Card.updateMany(
-          req.query.q ? JSON.parse(req.query.q) : {},
-          req.body
-        ).then((cards, err) => {
-          if (err) res.status(400).end();
-          else res.status(200).send(cards);
+    case "POST":
+      const newCard = new mongoose.models.Card(req.body);
+      await newCard
+        .save()
+        .then(() => {
+          res.status(201).send(newCard);
+        })
+        .catch((error) => {
+          res.status(400).end();
         });
-        break;
+      break;
 
-      case "DELETE":
-        Card.deleteMany(req.query.q ? JSON.parse(req.query.q) : {}).then(
-          (cards, err) => {
-            if (err) res.status(400).end();
-            else res.status(204).end();
-          }
-        );
-        break;
-    }
-  } catch (err) {
-    res.send(err);
+    case "PUT":
+      mongoose.models.Card.updateMany(
+        req.query.q ? JSON.parse(req.query.q) : {},
+        req.body
+      ).then((cards, err) => {
+        if (err) res.status(400).end();
+        else res.status(200).send(cards);
+      });
+      break;
+
+    case "DELETE":
+      mongoose.models.Card.deleteMany(
+        req.query.q ? JSON.parse(req.query.q) : {}
+      ).then((cards, err) => {
+        if (err) res.status(400).end();
+        else res.status(204).end();
+      });
+      break;
   }
 };
 
