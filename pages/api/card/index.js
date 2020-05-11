09363215e8ecab5f21702e41cc339027eadd1757
Oliver Butler <olly@protoncloud.co.uk>
2020-05-11 15:14:10 +0100

    🚀 Test Vercel deployment

--- pages/api/card/index.js
+++ pages/api/card/index.js
@@ -1,46 +1,51 @@
 import mongoose from "mongoose";
 import dbConnect from "../../../middlewares/db";
-import auth0 from "../../../lib/auth0";
 
 const { Card } = mongoose.models;
 
 const handler = async (req, res) => {
-  switch (req.method) {
-    case "GET":
-      const cards = await Card.find(req.query.q ? JSON.parse(req.query.q) : {});
-      res.send(cards);
-      break;
+  try {
+    switch (req.method) {
+      case "GET":
+        const cards = await Card.find(
+          req.query.q ? JSON.parse(req.query.q) : {}
+        );
+        res.send(cards);
+        break;
 
-    case "POST":
-      const newCard = new Card(req.body);
-      await newCard
-        .save()
-        .then(() => {
-          res.status(201).send(newCard);
-        })
-        .catch((error) => {
-          res.status(400).end();
-        });
-      break;
+      case "POST":
+        const newCard = new Card(req.body);
+        await newCard
+          .save()
+          .then(() => {
+            res.status(201).send(newCard);
+          })
+          .catch((error) => {
+            res.status(400).end();
+          });
+        break;
 
-    case "PUT":
-      Card.updateMany(
-        req.query.q ? JSON.parse(req.query.q) : {},
-        req.body
-      ).then((cards, err) => {
-        if (err) res.status(400).end();
-        else res.status(200).send(cards);
-      });
-      break;
-
-    case "DELETE":
-      Card.deleteMany(req.query.q ? JSON.parse(req.query.q) : {}).then(
-        (cards, err) => {
+      case "PUT":
+        Card.updateMany(
+          req.query.q ? JSON.parse(req.query.q) : {},
+          req.body
+        ).then((cards, err) => {
           if (err) res.status(400).end();
-          else res.status(204).end();
-        }
-      );
-      break;
+          else res.status(200).send(cards);
+        });
+        break;
+
+      case "DELETE":
+        Card.deleteMany(req.query.q ? JSON.parse(req.query.q) : {}).then(
+          (cards, err) => {
+            if (err) res.status(400).end();
+            else res.status(204).end();
+          }
+        );
+        break;
+    }
+  } catch (err) {
+    res.send(err);
   }
 };
 
