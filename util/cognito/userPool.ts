3a66cb5a0aea6cb101b992aa6d92e30e8f34c002
Oliver Butler <dev@oliverbutler.uk>
2020-05-16 02:07:50 +0100

    ⚗ Implemented DynamoDB helper utils

--- util/cognito/userPool.ts
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-  UserPoolId: "eu-west-2_hnNS8DXpa",
-  ClientId: "60n8v5u8un86kqmv3tnihfck5k",
-};
-
-export default new CognitoUserPool(poolData);
