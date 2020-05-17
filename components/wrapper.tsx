25a8879d49d6012ecaaeb2f0bedb18c9f4257014
Oliver Butler <dev@oliverbutler.uk>
2020-05-18 00:08:22 +0100

    🔍

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -1,6 +1,7 @@
+// @ts-nocheck
 import { AccountContext } from "context/account";
 import { useContext, useEffect } from "react";
-
+import { initGA, logPageView } from "util/analytics";
 import Auth from "@aws-amplify/auth";
 import Amplify from "@aws-amplify/core";
 import { awsConfig } from "awsConfig";
@@ -8,6 +9,14 @@ Amplify.configure(awsConfig);
 
 const Wrapper = ({ children }) => {
   const { state, dispatch } = useContext(AccountContext);
+
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
 
   useEffect(() => {
     async function getUser() {
