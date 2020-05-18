cf63d7e167b8e63366cb07e5ee2b95575e502b0d
Jwcode-uk <Jonathanwhite0000@gmail.com>
2020-05-18 01:33:34 +0100

    added regex checks on password

--- components/passwordCheck.tsx
+++ components/passwordCheck.tsx
@@ -2,21 +2,48 @@ import { useEffect, useState } from "react";
 import classnames from "classnames";
 
 const passportCheck = ({ password }) => {
+
   return (
     <progress
       className={
+
+          
+
         "progress is-small " +
         classnames({
-          "is-success": password.length > 8,
-          "is-warning": password.length < 9,
-          "is-danger": password.length < 6,
+          "is-success": passwordStrength(password) > 80,
+          "is-warning": passwordStrength(password) < 80,
+          "is-danger": passwordStrength(password) < 60,
         })
       }
+      value= {passwordStrength(password)}
+
+      // change class name here 
       style={{ height: "5px" }}
-      value={password.length * 8}
+      
+      
       max="100"
     ></progress>
   );
 };
 
+function passwordStrength(pw)
+{
+
+  var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
+  var mediumRegex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])");
+  var lowRegex = new RegExp("^((?=.*[a-z])(?=.*[A-Z]))|((?=.*[0-9])(?=.*[a-z]))|((?=.*[0-9])(?=.*[A-Z]))");
+  var multiplier = 2;
+if(strongRegex.test(pw)) {
+    multiplier=8;
+} 
+else if(mediumRegex.test(pw)) {
+    multiplier=6;
+} 
+else if(lowRegex.test(pw))
+{
+  multiplier=4;
+}
+return multiplier*pw.length;
+}
 export default passportCheck;
