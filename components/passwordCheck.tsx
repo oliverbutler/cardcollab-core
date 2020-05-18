06f87fc6a302dd11a5829d28be5f2b8b526fe523
Jwcode-uk <Jonathanwhite0000@gmail.com>
2020-05-18 01:53:20 +0100

    formatted code

--- components/passwordCheck.tsx
+++ components/passwordCheck.tsx
@@ -2,13 +2,9 @@ import { useEffect, useState } from "react";
 import classnames from "classnames";
 
 const passportCheck = ({ password }) => {
-
   return (
     <progress
       className={
-
-          
-
         "progress is-small " +
         classnames({
           "is-success": passwordStrength(password) >= 80,
@@ -16,34 +12,32 @@ const passportCheck = ({ password }) => {
           "is-danger": passwordStrength(password) < 60,
         })
       }
-      value= {passwordStrength(password)}
-
-      // change class name here 
+      value={passwordStrength(password)}
+      // change class name here
       style={{ height: "5px" }}
-      
-      
       max="100"
     ></progress>
   );
 };
 
-function passwordStrength(pw)
-{
-
-  var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
-  var mediumRegex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])|(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])| (?=.*[0-9])(?=.*[a-z])(?=.*[!@#\$%\^&\*])");
-  var lowRegex = new RegExp("^((?=.*[a-z])(?=.*[A-Z]))|((?=.*[0-9])(?=.*[a-z]))|((?=.*[0-9])(?=.*[A-Z]))");
+function passwordStrength(pw) {
+  var strongRegex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])"
+  );
+  var mediumRegex = new RegExp(
+    "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])|(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])| (?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*])"
+  );
+  var lowRegex = new RegExp(
+    "^((?=.*[a-z])(?=.*[A-Z]))|((?=.*[0-9])(?=.*[a-z]))|((?=.*[0-9])(?=.*[A-Z]))"
+  );
   var multiplier = 2;
-if(strongRegex.test(pw)) {
-    multiplier=8;
-} 
-else if(mediumRegex.test(pw)) {
-    multiplier=6;
-} 
-else if(lowRegex.test(pw))
-{
-  multiplier=4;
-}
-return multiplier*pw.length;
+  if (strongRegex.test(pw)) {
+    multiplier = 8;
+  } else if (mediumRegex.test(pw)) {
+    multiplier = 6;
+  } else if (lowRegex.test(pw)) {
+    multiplier = 4;
+  }
+  return multiplier * pw.length;
 }
 export default passportCheck;
