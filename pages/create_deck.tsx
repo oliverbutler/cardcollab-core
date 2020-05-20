716c4c6e14980cab39002d09351d150590663847
Oliver Butler <dev@oliverbutler.uk>
2020-05-20 17:03:35 +0100

    💄 Added create a new deck form

--- pages/create_deck.tsx
+++ pages/create_deck.tsx
@@ -1,27 +1,53 @@
 import { useState } from "react";
+import Input, { InputType } from "components/input";
+import { motion } from "framer-motion";
 
 const create_deck = () => {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [subject, setSubject] = useState("");
+  const [module, setModule] = useState("");
+  const [file, setFile] = useState();
 
   return (
     <div className="container">
-      <h1 className="title">Create a New Deck</h1>
-      <form className="is-narrow">
-        <div className="field">
-          <label className="label">Verify Password</label>
-          <div className="control has-icons-left">
-            <input
-              className="input"
-              type="text"
+      <div className="columns is-centered is-vcentered is-mobile">
+        <div className="column is-narrow">
+          <h1 className="title" style={{}}>
+            Create a New Deck
+          </h1>
+          <form>
+            <Input
+              type={InputType.TEXT}
+              onChange={setTitle}
               value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <span className="icon is-small is-left">
-              <ion-icon name="document-outline"></ion-icon>
-            </span>
-          </div>
+              title="Title"
+              iconLeft="albums-outline"
+            ></Input>
+            <Input
+              type={InputType.TEXT}
+              onChange={setDescription}
+              value={description}
+              title="Description"
+              iconLeft="ellipsis-horizontal-outline"
+            ></Input>
+            <Input
+              type={InputType.TEXT}
+              onChange={setSubject}
+              value={subject}
+              title="Subject"
+              iconLeft="calculator-outline"
+            ></Input>
+            <Input
+              type={InputType.TEXT}
+              onChange={setModule}
+              value={module}
+              title="Module"
+              iconLeft="school-outline"
+            ></Input>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
