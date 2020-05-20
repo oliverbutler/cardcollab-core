f5331d955e71a1464cb4cd9a23cb9e47eecb5b84
Jwcode-uk <Jonathanwhite0000@gmail.com>
2020-05-20 01:21:05 +0100

    made deckCard component reusable

--- pages/decks.tsx
+++ pages/decks.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import { logPageView } from "util/analytics";
 import { addCard } from "util/db/card";
+import { Deck } from "components/deckCard";
 const decks = () => {
 
   
@@ -59,31 +60,6 @@ const decks = () => {
   );
 };
 
-function Deck (props)  {
-  return (
-    
-            <div className="card" style={{ height: "100%" }}>
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <img
-                    src={props.c.url ? props.c.url : ""}
-                    alt="Placeholder image"
-                    height={300}
-                  />
-                </figure>
-              </div>
-              <div className="card-content">
-                <h1 className="title is-4">{props.c.title}</h1>
-                <p className="subtitle">
-                  @{props.c.author}
-                  <span className="tag is-success is-light"> Admin</span>
-                </p>
 
-                <p>{props.c.description}</p>
-              </div>
-            </div>
-
-  );
-};
 
 export default decks;
