51d87cb246a6d678b58fa1ab80e4424b86822257
Oliver Butler <dev@oliverbutler.uk>
2020-05-25 00:45:44 +0100

    🎨 Improved card utils

--- util/db/card.ts
+++ util/db/card.ts
@@ -1,5 +1,6 @@
 import nanoid from "nanoid";
 import AWS from "aws-sdk";
+import { isEmpty } from "util/functions";
 
 AWS.config.update({
   region: "eu-west-2",
@@ -15,17 +16,79 @@ var docClient = new AWS.DynamoDB.DocumentClient();
  * @param answer - Answer
  */
 export const addCard = (deckID: string, question: string, answer: string) => {
+  const cardID = nanoid.nanoid();
   var params: AWS.DynamoDB.DocumentClient.PutItemInput = {
     TableName: "CardCollab",
     Item: {
       partitionKey: `deck#${deckID}`,
-      sortKey: "card#" + nanoid.nanoid(),
+      sortKey: "card#" + cardID,
       question: question,
       answer: answer,
     },
   };
 
-  return docClient.put(params).promise();
+  return docClient
+    .put(params)
+    .promise()
+    .then((res) => {
+      return cardID;
+    });
+};
+
+/**
+ * Returns all the cards of a deck in an array
+ *
+ * @param deckID
+ */
+export const getCards = (deckID: string) => {
+  var params: AWS.DynamoDB.DocumentClient.QueryInput = {
+    TableName: "CardCollab",
+    KeyConditionExpression: "partitionKey = :pk and begins_with(sortKey, :sk)",
+    ExpressionAttributeValues: {
+      ":pk": `deck#${deckID}`,
+      ":sk": "card#",
+    },
+  };
+  return docClient
+    .query(params)
+    .promise()
+    .then((res) => {
+      var cards = res.Items.map((value) => {
+        return {
+          cardID: value.sortKey.substring(5, value.sortKey.length),
+          question: value.question,
+          answer: value.answer,
+        };
+      });
+      return cards;
+    });
+};
+
+/**
+ * Get an individual card
+ *
+ * @param deckID
+ * @param cardID
+ */
+export const getCard = (deckID: string, cardID: string) => {
+  var params: AWS.DynamoDB.DocumentClient.GetItemInput = {
+    TableName: "CardCollab",
+    Key: {
+      partitionKey: `deck#${deckID}`,
+      sortKey: `card#${cardID}`,
+    },
+  };
+  return docClient
+    .get(params)
+    .promise()
+    .then((value) => {
+      if (isEmpty(value.Item)) throw new Error("No Card Found");
+      return {
+        cardID: value.Item.sortKey.substring(5, value.Item.sortKey.length),
+        question: value.Item.question,
+        answer: value.Item.answer,
+      };
+    });
 };
 
 /**
@@ -42,7 +105,12 @@ export const deleteCard = (deckID: string, cardID: string) => {
       sortKey: "card#" + cardID,
     },
   };
-  return docClient.delete(params).promise();
+  return docClient
+    .delete(params)
+    .promise()
+    .then((res) => {
+      return "Successfully deleted card";
+    });
 };
 
 /**
