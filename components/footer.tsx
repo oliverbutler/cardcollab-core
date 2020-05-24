caa0e2c222e1370bb736c228e975b6d0ab14150f
Jwcode-uk <Jonathanwhite0000@gmail.com>
2020-05-24 23:51:21 +0100

    improved footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,44 +1,57 @@
 import styles from "./footer.module.scss";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 const Footer = () => {
+  const [mode, setMode] = useState(false);
+
   return (
-    <footer>
-      <div className="columns">
+    <footer className={styles.Footer}>
+      <div className="columns ">
         <div className="column">
-          <button className="button is-light">Dark Mde</button>
+          <Switch mode={mode} setMode={setMode} />
         </div>
         <div className="column">
           <img width="50px" height="50px" alt="logo" src="/logo.svg" />
           <p>Copyright © 2020</p>
         </div>
         <div className="column">
-          <div>
-            <a href="#">
-              <button className="button is-primary">
-                <FontAwesomeIcon color="white" icon="coffee" />
-              </button>
-            </a>
-            <a href="#">
-              <button className="button is-primary">
-                <span className="icon is-small">twitter</span>
-              </button>
-            </a>
-            <a href="#">
-              <button className="button is-primary">
-                <span className="icon is-small">YT</span>
-              </button>
-            </a>
-            <a href="#">
-              <button className="button is-primary">
-                <span className="icon is-small">github</span>
-              </button>
-            </a>
-          </div>
+          <button className="button is-primary">
+            <ion-icon name="logo-facebook"></ion-icon>
+          </button>
+
+          <button className="button is-primary">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </button>
+
+          <button className="button is-primary">
+            <ion-icon name="logo-instagram"></ion-icon>
+          </button>
+
+          <button className="button is-primary">
+            <ion-icon name="logo-github"></ion-icon>
+          </button>
         </div>
       </div>
     </footer>
   );
+};
+
+const Switch = (prop) => {
+  if (prop.mode) {
+    return (
+      <button className="button is-dark" onClick={() => prop.setMode(false)}>
+        Dark Mode
+      </button>
+    );
+  } else {
+    return (
+      <button className="button is-light" onClick={() => prop.setMode(true)}>
+        Light Mode
+      </button>
+    );
+  }
 };
 
 export default Footer;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,47 @@
 import { logPageView } from "util/analytics";
+import { Deck } from "components/deckCard";
+
+const decks = [
+  {
+    title: "Algorithms and Design",
+    author: "Jonno",
+    description: "Algorithm and design flash cards for CSC2023",
+    url:
+      "https://images.unsplash.com/photo-1551033406-611cf9a28f67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80",
+  },
+  {
+    title: "Operating Systems",
+    author: "olly",
+    description: "Operating systems module",
+    url:
+      "https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+  },
+  {
+    title: "Web Development",
+    author: "olly",
+    description: "Web development with XHTML",
+    url:
+      "https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+  },
+];
 
 const about = () => {
   logPageView("/about");
   return (
     <div className="container">
-      <h1 className="title">About Page</h1>
+      <h1 className="title">What is Card Collab</h1>
+      <p className="subtitle">
+        CardCollab is a start up founded by 4 students who were fed up of over
+        complated and distracting revision tools online so as a result we came
+        together andcreated card collab a minualist revision enviroment
+      </p>
+      <div className="columns is-multiline">
+        {decks.map((card) => (
+          <div className="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen">
+            <Deck c={card}></Deck>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
